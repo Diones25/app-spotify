690f05d4ef69463c0557a9791c7dd18c56c85be4
Diones <pereiradiones987@gmail.com>
2025-11-22 18:23:07 -0300

    corrigindo scopo

--- backend/src/controllers/spotify.ts
+++ backend/src/controllers/spotify.ts
@@ -24,7 +24,7 @@ const login = async (req: Request, res: Response) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private user-soa-link user-soa-unlink soa-manage-entitlements soa-manage-partner soa-create-partner";
+  const scope = "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
     querystring.stringify({
