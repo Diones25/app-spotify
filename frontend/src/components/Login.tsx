ee07113f0b28220dfd6ab263dc5dcbf9ec1d47a5
Diones <pereiradiones987@gmail.com>
2025-11-23 14:53:17 -0300

    atualizando página de login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,25 +1,39 @@
 
 import { Link } from 'react-router-dom';
-import iconSpofity from '../assets/spotify.svg';
+import Logo from "../../public/logo.png";
+import { Button } from './ui/button';
 
 const Login = () => {
   return (
-    <>
-      <div className="flex items-center justify-center min-h-screen bg-green-100">
-        
-        <div className='flex justify-center flex-col'>
-
-          <div className='flex items-center justify-center mb-8'>
-            <img src={iconSpofity} className='h-[60px] mr-3' alt="icone spotify" />
-            <span className='text-5xl font-bold text-[#10BC4C]'>Spotify</span>
-          </div>
-
-          <Link to={"http://localhost:8888/login"} className="bg-green-400 text-center text-white text-lg rounded-full py-3 font-semibold">
-            Login com Spotify
-          </Link>
+    <div
+      className="relative flex justify-center items-center w-screen h-screen"
+      style={{
+        backgroundImage: "url('/thumb-spotify.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage:
+            'linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.3))',
+        }}
+      ></div>
+      <div className="relative z-10 w-xl flex flex-col items-center">
+        <img src={Logo} alt="Logo Spotify" className="w-90" />
+        <div className="text-center my-3">
+          <h2 className="text-white text-6xl">Milhões de músicas</h2>
+          <h2 className="text-white text-6xl">grátis no Spotify</h2>
         </div>
+        <Link to={"http://localhost:8888/login"} className="w-full">
+          <Button className="mt-5 cursor-pointer bg-[#57e453] hover:bg-[#57e453] text-black w-full h-[60px] rounded-full text-2xl">
+            Login com Spotify
+          </Button>
+        </Link>
       </div>
-    </>
+    </div>
   )
 }
 
