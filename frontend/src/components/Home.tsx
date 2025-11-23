8eb36cba5fb1de1e3f80ba4fddc51271bdf7bf8c
Diones <pereiradiones987@gmail.com>
2025-11-23 16:39:13 -0300

    atualizando home

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { TokenContext } from "../contexts/TokenContext";
 import { requester } from "../utils/api";
 import type { UserProfile } from "@/types/UserProfile";
+import LogoIcon from "../../public/logo_icon.png";
 
 const Home = () => {
   const tokenCtx = useContext(TokenContext);
@@ -27,7 +28,23 @@ const Home = () => {
 
   return (
     <>
-      <h1>Home - Bem-vindo(a), {me?.display_name || 'usuário'}!</h1>
+      <div className="bg-red-300 h-12 mt-2 mb-2 flex items-center">
+        <div>
+          <img src={LogoIcon} alt="" className="w-12" />
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      <h1 className="text-white">Home - Bem-vindo(a), {me?.display_name || 'usuário'}!</h1>
     </>
   )
 }
