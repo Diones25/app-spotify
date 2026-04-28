b77326a93d607cb2917a891831adf28dbcfbcb15
Diones <pereiradiones987@gmail.com>
2026-04-27 23:50:17 -0300

    feat: add component Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Página Inicial</h1>
+      <Button>Entrar com Google</Button>
     </div>
   );
 }
