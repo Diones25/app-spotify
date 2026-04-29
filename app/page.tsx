5bd25e4e74404a8ebc19e1199a0fb7f701c2d6e5
Diones <pereiradiones987@gmail.com>
2026-04-28 23:46:26 -0300

    feat: layout inicial do login

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { LoginGoogle } from "@/components/login-google";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Página Inicial</h1>
-      <Button>Entrar com Google</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginGoogle />
+      </div>
     </div>
   );
 }
