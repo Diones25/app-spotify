7018291da193205ff81c6c941f2b4c14919261cc
Diones <pereiradiones987@gmail.com>
2026-05-03 15:03:13 -0300

    feata: autalizando card

--- components/SpotifyCard.tsx
+++ components/SpotifyCard.tsx
@@ -24,14 +24,14 @@ export function SpotifyCard({ title, subtitle, image, type = "playlist" }: CardP
         )}
         
         {/* Play Button Overlay */}
-        <div className="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-[-8px] transition-all duration-300 translate-y-0">
+        <div className="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 group-hover:-translate-y-2 transition-all duration-300 translate-y-0">
           <button className="bg-[#1ed760] p-3 rounded-full shadow-xl hover:scale-105 active:scale-95 text-black">
             <Play fill="black" size={24} />
           </button>
         </div>
       </div>
       
-      <div className="min-h-[62px]">
+      <div className="min-h-15.5">
         <h3 className="text-white font-bold truncate mb-1">{title}</h3>
         <p className="text-[#b3b3b3] text-sm line-clamp-2">{subtitle}</p>
       </div>
